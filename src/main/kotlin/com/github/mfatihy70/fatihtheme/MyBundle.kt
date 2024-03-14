afae85bc535be9409f49abfa41ad34a592af5751
GitHub Action <action@github.com>
2024-03-14 10:23:05 +0000

    Template cleanup

--- src/main/kotlin/com/github/mfatihy70/fatihtheme/MyBundle.kt
+++ src/main/kotlin/com/github/mfatihy70/fatihtheme/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.mfatihy70.fatihtheme
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/mfatihy70/fatihtheme/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/mfatihy70/fatihtheme/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.mfatihy70.fatihtheme.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
